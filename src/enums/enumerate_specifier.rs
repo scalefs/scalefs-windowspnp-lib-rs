f269776d2e0c1905b1dbe8f8ac7017a035096066
kit-scalefs <154656609+kit-scalefs@users.noreply.github.com>
2023-12-30 16:47:42 -0400

    Fix typo

--- src/enums/enumerate_specifier.rs
+++ src/enums/enumerate_specifier.rs
@@ -6,8 +6,8 @@ use windows_sys::core::GUID;
 // #[derive(Clone)]
 pub enum EnumerateSpecifier {
     AllDevices,
-    DeviceInterfaceClassGuid(/*device_interface_class_guide: */GUID),
-    DeviceSetupClassGuid(/*device_setup_class_guide: */GUID),
+    DeviceInterfaceClassGuid(/*device_interface_class_guid: */GUID),
+    DeviceSetupClassGuid(/*device_setup_class_guid: */GUID),
     PnpDeviceInstanceId(/*device_instance_id: */String, /*device_interface_class_guid: */Option<GUID>),
     PnpEnumeratorId(/*enumerator_id: */String),
 }
